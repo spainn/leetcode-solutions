9be9ceeae61a497cdb70501a83d14494db15bb29
spainn <148598154+spainn@users.noreply.github.com>
2025-01-31 00:18:01 -0600

    added a second solution to 392.py that removes the need for checking edge cases.  runs just as fast as both are O(n) time

--- 392.py
+++ 392.py
@@ -17,3 +17,17 @@ class Solution:
                     return True
 
         return False
+
+
+# more compact solution using the same principles in a while loop
+# removes the need for checking edge cases at the top of the function
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+
+        s_pointer, t_pointer = 0, 0
+        while t_pointer < len(t) and s_pointer < len(s):
+            if s[s_pointer] == t[t_pointer]:
+                s_pointer += 1
+            t_pointer += 1
+
+        return s_pointer == len(s)
